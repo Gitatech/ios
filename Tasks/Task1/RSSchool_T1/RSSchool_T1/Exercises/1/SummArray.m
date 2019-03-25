fb173d33b283a1cb8a485a80714e4227692d5e1b
Gitatech <gorazio.nov@gmail.com>
2019-03-25 16:08:12 +0300

    feat: Ex1 was completed

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/1/SummArray.m
@@ -1,10 +1,18 @@
 #import "SummArray.h"
 
 @implementation SummArray
-
 // Complete the summArray function below.
 - (NSNumber *)summArray:(NSArray *)array {
-    return @(0);
+    NSInteger summ = 0;
+    NSLog(@"Array: %@", array);
+    
+    // get the summ of array
+    for (NSNumber *item in array) {
+        summ += [item integerValue];
+    }
+    
+    NSLog(@"Summ = %ld", (long)summ);
+    return [NSNumber numberWithInteger:summ];
 }
 
 @end
