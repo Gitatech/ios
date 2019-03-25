3212ca03249039e11d144f1e6fbc9662eff8f34a
Gitatech <gorazio.nov@gmail.com>
2019-03-25 23:12:22 +0300

    feat: methods were initialized

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/5/Sorted.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/5/Sorted.m
@@ -8,7 +8,48 @@
 // Complete the sorted function below.
 - (ResultObject*)sorted:(NSString*)string {
     ResultObject *value = [ResultObject new];
+    NSArray *array = [string componentsSeparatedByString:@" "];
+
+    if ([Sorted isSorted:array]) {
+        value.status = YES;
+    }
+    
+    // TODO:
+    [Sorted swapArray:array];
+    // TODO:
+    [Sorted reverseArray:array];
+    
+    if ([Sorted isSorted:[Sorted swapArray:array]]) {
+        value.status = YES;
+    } else if ([Sorted isSorted:[Sorted reverseArray:array]]) {
+        value.status = YES;
+    }
+    value.status = NO;
     return value;
+}
+
+// check, if inputArray is sorted or not
++ (BOOL)isSorted:(NSArray *)inputArray {
+    if ([inputArray count]<=1) {
+        return YES;
+    }
+    
+    for (int i=1; i<[inputArray count]; i++) {
+        if ([inputArray[i-1] integerValue]>[inputArray[i] integerValue]) {
+            return  NO;
+        }
+    }
+    return YES;
+}
+
+// check, if inputArray could be sorted by swapping items
++ (NSArray *)swapArray:(NSArray *)inputArray {
+    return @[];
+}
+
+// check, if inputArray could be sorted by reversing components
++ (NSArray *)reverseArray:(NSArray *)inputArray {
+    return @[];
 }
 
 @end
