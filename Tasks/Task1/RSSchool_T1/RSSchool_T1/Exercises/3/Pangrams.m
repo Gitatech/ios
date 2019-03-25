efe501bba073b7a8e8c796e3271dab432943c9aa
Gitatech <gorazio.nov@gmail.com>
2019-03-25 20:35:21 +0300

    feat: Ex3 was completed via NSRange

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -4,7 +4,27 @@
 
 // Complete the pangrams function below.
 - (BOOL)pangrams:(NSString *)string {
-    return NO;
+    NSString *alphabet = @"abcdefghijklmnopqrstuvwxyz"; // set up the alphabet string
+    Boolean flag = YES;
+    
+//    NSString *rr = @"Hello world 123456789!";
+//    NSLog(@"!!! %@", [rr substringWithRange:NSMakeRange(6, 13)]);
+//
+    for (int item=0; item<[alphabet length]; item++) {
+        // get each symbol from the alphabet string
+        // take a substring of length 1 symbol from the position 'item'
+        NSString *symbol = [alphabet substringWithRange:NSMakeRange(item, 1)];
+        // compare input string on containig of the alphabet item
+        if (![[string lowercaseString] containsString:symbol]) {
+            flag = NO;
+        }
+    }
+    
+//    NSCharacterSet *set = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyz"];
+//    if ([[string lowercaseString] rangeOfCharacterFromSet:set].location == NSNotFound) {
+//        flag = NO;
+//    }
+    return flag;
 }
 
 @end
