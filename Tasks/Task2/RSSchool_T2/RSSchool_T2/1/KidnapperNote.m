f97e2475248340a4fb68d14b6cf001400b1032cd
Gitatech <gorazio.nov@gmail.com>
2019-04-01 14:50:52 +0300

    feat: Ex1 was completed

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -2,4 +2,24 @@
 
 @implementation KidnapperNote
 // your code here
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    
+    // create arrays with words from string
+    NSMutableArray *magazineWords = [[magaine.lowercaseString copy] componentsSeparatedByString:@" "];
+    NSMutableArray *noteWords = [[note.lowercaseString copy] componentsSeparatedByString:@" "];
+    
+    // create counted sets for comparing items
+    NSCountedSet *MagazineSet = [NSCountedSet.new initWithArray:magazineWords];
+    NSCountedSet *noteSet = [NSCountedSet.new initWithArray:noteWords];
+    
+    // remove all objects from note if they are present
+    [noteSet minusSet:MagazineSet];
+    NSInteger counter = noteSet.count;
+
+    if (counter==0) {
+        return YES;
+    }
+    return NO;
+}
+
 @end
