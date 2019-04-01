605663f974bde9cce29342c71194140bfb592245
Gitatech <gorazio.nov@gmail.com>
2019-04-01 23:02:01 +0300

    feat: Ex4 was completed

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/4/MatrixHacker.m
@@ -1,5 +1,48 @@
 #import "MatrixHacker.h"
-// your code here
+
+//@interface Character : NSObject <Character>
+//
+//@property (nonatomic, assign, getter=isClone) BOOL clone;
+//@property (nonatomic, copy) NSString *name;
+//
+//@end
+
+typedef id<Character> (^searchBlock)(NSString *name);
+
+@interface MatrixHacker()
+
+@property (nonatomic, copy) searchBlock myBlock;
+
+@end
+
 @implementation MatrixHacker
-// your code here
+
+- (void)injectCode:(id<Character> (^)(NSString *))theBlock {
+    _myBlock = theBlock;
+}
+
+- (NSArray<id<Character>> *)runCodeWithData:(NSArray<NSString *> *)names {
+    NSMutableArray<id<Character>> *array = [[NSMutableArray new] autorelease];
+    for (NSString *name in names) {
+//        if (_myBlock != nil) {
+            id<Character> myCharacter = _myBlock(name);
+            if (_myBlock != nil && [myCharacter.class respondsToSelector:@selector(createWithName:isClone:)]) {
+                if ([name isEqualToString:@"Neo"]) {
+                    myCharacter = [myCharacter.class createWithName:name isClone:NO];
+                } else {
+                    myCharacter = [myCharacter.class createWithName:name isClone:YES];
+                }
+            }
+            [array addObject:myCharacter];
+//        }
+    }
+    return array;
+}
+
+- (void)dealloc
+{
+    [_myBlock release];
+    [super dealloc];
+}
+
 @end
