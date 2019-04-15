1756e86743818e9a6173bdedbe6a2efc81f2d3e1
Gitatech <gorazio.nov@gmail.com>
2019-04-15 17:35:58 +0300

    feat: Ex1 was completed

--- Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
+++ Tasks/RSSchool_T3/RSSchool_T3/1/ArrayPrint.m
@@ -1,1 +1,41 @@
 #import "ArrayPrint.h"
+
+@implementation NSArray (RSSchool_Extension_Name)
+
+//Input: @[@[@0, @1, @2], [NSNull null], @[@"123",@"456",@"789"], @[@[@[@1337], @{@"Key": @"Value"}]]]
+//Output: [[0,1,2],null,["123","456","789"],[[[1337],unsupported]]]
+
+//NSNumber
+//NSNull
+//NSArray
+//NSString
+// unsupported
+
+-(NSString *)print {
+    NSMutableArray *resultArray = [[NSMutableArray new] autorelease];
+    for (id item in self) {
+        if ([item isKindOfClass:[NSNumber class]]) {
+            NSLog(@"NSNumber item = %@", item);
+            [resultArray addObject:[NSString stringWithFormat:@"%@", item]];
+        } else
+            if ([item isKindOfClass:[NSNull class]]) {
+                NSLog(@"NSNull item = %@", item);
+                [resultArray addObject:[NSString stringWithFormat:@"%@", @"null"]];
+            } else
+                if ([item isKindOfClass:[NSArray class]]) {
+                    NSLog(@"NSArray item = %@", item);
+                    [resultArray addObject:[item print]];
+                } else
+                    if ([item isKindOfClass:[NSString class]]) {
+                        NSLog(@"NSString item = %@", item);
+                        [resultArray addObject:[NSString stringWithFormat:@"\"%@\"", item]];
+                    } else
+                        [resultArray addObject: [NSString stringWithFormat:@"%@", @"unsupported"]];
+    }
+
+    NSString *resultString = [NSString stringWithFormat:@"%@%@%@", @"[", [resultArray componentsJoinedByString:@","], @"]"];
+    NSLog(@"%@", resultString);
+    return resultString;
+}
+
+@end
