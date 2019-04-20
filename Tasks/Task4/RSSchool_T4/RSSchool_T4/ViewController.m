46cd6d0e9d477035c980ad321d4ac78a2b5898fb
Gitatech <gorazio.nov@gmail.com>
2019-04-20 14:55:32 +0300

    feat: the determination of the Country Code was added

--- Tasks/Task4/RSSchool_T4/RSSchool_T4/ViewController.m
+++ Tasks/Task4/RSSchool_T4/RSSchool_T4/ViewController.m
@@ -10,7 +10,9 @@
 //1. Код страны не входит в длину номера телефона
 //2. Максимальная длина номера телефона 12 знаков (без форматирования)
 //При вводе номера телефона автоматически подставляется “+” перед первым символом если пользователь сам не ввел “+” при наборе номера телефона.
-//Если код страны определился то в левой части UITextField должен отобразится флаг страны. 
+//Если код страны определился то в левой части UITextField должен отобразится флаг страны.
+
+//используем frame/bounds для работы с размерами элементов
 
 #import "ViewController.h"
 
@@ -29,18 +31,19 @@
 }
 
 -(void)setupView {
-    self.view.backgroundColor = [UIColor colorWithRed:0.0 green:0.38 blue:0.54 alpha:1.0];
+    self.view.backgroundColor = [UIColor colorWithRed:0.0 green:0.38 blue:0.54 alpha:0.9];
     NSLog(@"ViewController was setuped");
     [self setupTextFieldAppearance];
+    [self defineCountryCode:@"380748234952379428379238749"]; // just check algorithm
 }
 
 -(void)setupTextFieldAppearance {
     CGRect textFieldSize = CGRectMake(20, 40, self.view.frame.size.width - 40, 60);
-    self.phoneNumberTextField = [[[UITextField alloc] initWithFrame:textFieldSize]autorelease];
-    self.phoneNumberTextField.font = [UIFont boldSystemFontOfSize:15];
+    self.phoneNumberTextField = [[[UITextField alloc] initWithFrame:textFieldSize] autorelease];
+    self.phoneNumberTextField.font = [UIFont boldSystemFontOfSize:21];
     self.phoneNumberTextField.textColor = UIColor.blackColor;
     self.phoneNumberTextField.placeholder = @"Enter the phone number";
-    self.phoneNumberTextField.backgroundColor = UIColor.lightGrayColor;
+    self.phoneNumberTextField.backgroundColor = UIColor.whiteColor;
     self.phoneNumberTextField.leftViewMode = UITextFieldViewModeAlways;
     self.phoneNumberTextField.autocorrectionType = UITextAutocorrectionTypeNo;
     self.phoneNumberTextField.keyboardType = UIKeyboardTypePhonePad;
@@ -49,22 +52,86 @@
     self.phoneNumberTextField.delegate = self;
     self.phoneNumberTextField.borderStyle = UITextBorderStyleLine;
     [self.view addSubview:self.phoneNumberTextField];
-
-    UIView *imageContainer = [[UIView alloc] initWithFrame:CGRectMake(self.phoneNumberTextField.frame.origin.x,
-                                                                      self.phoneNumberTextField.frame.origin.y,
-                                                                      self.phoneNumberTextField.frame.size.width/4,
-                                                                      self.phoneNumberTextField.frame.size.height - 10)];
-    self.flagImage = [[UIImageView alloc] initWithFrame:CGRectMake(5, 0,
-                                                                   imageContainer.frame.size.height,
-                                                                   imageContainer.frame.size.height)];
-//    self.flagImage.image = [UIImage imageNamed:@""];
+    
+    UIView *imageContainer = [[[UIView alloc] initWithFrame:CGRectMake(self.phoneNumberTextField.frame.origin.x,
+                                                                       self.phoneNumberTextField.frame.origin.y,
+                                                                       self.phoneNumberTextField.frame.size.width/4,
+                                                                       self.phoneNumberTextField.frame.size.height - 10)] autorelease];
+    self.flagImage = [[[UIImageView alloc] initWithFrame:CGRectMake(5, 0,
+                                                                    imageContainer.frame.size.height,
+                                                                    imageContainer.frame.size.height)] autorelease];
     self.flagImage.contentMode = UIViewContentModeScaleToFill;
     [imageContainer addSubview:self.flagImage];
     self.phoneNumberTextField.leftView = imageContainer;
 }
 
+#pragma mark - define the code of the country and setup country flag
+-(void)defineCountryCode:(NSString *)phoneNumber {
+//    NSString *flagName = [[NSString new] autorelease];
+    NSMutableString *prefix = [[NSMutableString new] autorelease];
+    if (phoneNumber.length >= 3) {
+        prefix = [NSMutableString stringWithFormat:@"%@", [phoneNumber substringWithRange:NSMakeRange(0, 3)]];
+    }
+    NSLog(@"prefix -> %@", prefix);
+    
+    switch ([prefix intValue]) {
+        case 998:
+            self.flagImage.image = [UIImage imageNamed:@"flag_UZ"];
+            break;
+        case 996:
+            self.flagImage.image = [UIImage imageNamed:@"flag_KG"];
+            break;
+        case 994:
+            self.flagImage.image = [UIImage imageNamed:@"flag_AZ"];
+            break;
+        case 993:
+            self.flagImage.image = [UIImage imageNamed:@"flag_TM"];
+            break;
+        case 992:
+            self.flagImage.image = [UIImage imageNamed:@"flag_TJ"];
+            break;
+        case 380:
+            self.flagImage.image = [UIImage imageNamed:@"flag_UA"];
+            break;
+        case 375:
+            self.flagImage.image = [UIImage imageNamed:@"flag_BY"];
+            break;
+        case 374:
+            self.flagImage.image = [UIImage imageNamed:@"flag_AM"];
+            break;
+        case 373:
+            self.flagImage.image = [UIImage imageNamed:@"flag_MD"];
+            break;
+        default:
+            if (phoneNumber.length >= 2) {
+                prefix = [NSMutableString stringWithFormat:@"%@", [phoneNumber substringWithRange:NSMakeRange(0, 2)]];
+            }
+            switch ([prefix intValue]) {
+                case 77:
+                    self.flagImage.image = [UIImage imageNamed:@"flag_KZ"];
+                    break;
+                default:
+                    if (phoneNumber.length >= 1) {
+                        prefix = [NSMutableString stringWithFormat:@"%@", [phoneNumber substringWithRange:NSMakeRange(0, 1)]];
+                    }
+                    switch ([prefix intValue]) {
+                        case 7:
+                            self.flagImage.image = [UIImage imageNamed:@"flag_RU"];
+                            break;
+                        default:
+                            NSLog(@"Country code not found. Check input data!");
+                            break;
+                    }
+                    break;
+            }
+            break;
+    }
+}
+
 - (void)dealloc
 {
+    [self phoneNumberTextField];
+    [self flagImage];
     [super dealloc];
 }
 
